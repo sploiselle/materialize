9701831f35b1b0a55fd415f535063d53ad50c4ef
Sean Loiselle <sean@materialize.io>
2022-07-26 12:05:35 -0400

    merge snapshot and listen with ReaderEnrichedHollowBatch

--- src/storage/src/source/persist_source.rs
+++ src/storage/src/source/persist_source.rs
@@ -47,140 +47,140 @@ use crate::types::sources::SourceData;
 // place the code that allows selecting from multiple `Listen`s, potentially by implementing async
 // `Stream` for it and then using `select_all`. And it will require us to properly combine the
 // upper frontier from all shards.
-pub fn persist_source<G>(
-    scope: &G,
-    persist_clients: Arc<Mutex<PersistClientCache>>,
-    metadata: CollectionMetadata,
-    as_of: Antichain<Timestamp>,
-) -> (
-    Stream<G, (Row, Timestamp, Diff)>,
-    Stream<G, (DataflowError, Timestamp, Diff)>,
-    Rc<dyn Any>,
-)
-where
-    G: Scope<Timestamp = mz_repr::Timestamp>,
-{
-    let worker_index = scope.index();
+// pub fn persist_source<G>(
+//     scope: &G,
+//     persist_clients: Arc<Mutex<PersistClientCache>>,
+//     metadata: CollectionMetadata,
+//     as_of: Antichain<Timestamp>,
+// ) -> (
+//     Stream<G, (Row, Timestamp, Diff)>,
+//     Stream<G, (DataflowError, Timestamp, Diff)>,
+//     Rc<dyn Any>,
+// )
+// where
+//     G: Scope<Timestamp = mz_repr::Timestamp>,
+// {
+//     let worker_index = scope.index();
 
-    // This source is split into two parts: a first part that sets up `async_stream` and a timely
-    // source operator that the continuously reads from that stream.
-    //
-    // It is split that way because there is currently no easy way of setting up an async source
-    // operator in materialize/timely.
+//     // This source is split into two parts: a first part that sets up `async_stream` and a timely
+//     // source operator that the continuously reads from that stream.
+//     //
+//     // It is split that way because there is currently no easy way of setting up an async source
+//     // operator in materialize/timely.
 
-    // This is a generator that sets up an async `Stream` that can be continously polled to get the
-    // values that are `yield`-ed from it's body.
-    let async_stream = async_stream::try_stream!({
-        // We are reading only from worker 0. We can split the work of reading from the snapshot to
-        // multiple workers, but someone has to distribute the splits. Also, in the glorious
-        // STORAGE future, we will use multiple persist shards to back a STORAGE collection. Then,
-        // we can read in parallel, by distributing shard reading amongst workers.
-        if worker_index != 0 {
-            trace!("We are not worker 0, exiting...");
-            return;
-        }
+//     // This is a generator that sets up an async `Stream` that can be continously polled to get the
+//     // values that are `yield`-ed from it's body.
+//     let async_stream = async_stream::try_stream!({
+//         // We are reading only from worker 0. We can split the work of reading from the snapshot to
+//         // multiple workers, but someone has to distribute the splits. Also, in the glorious
+//         // STORAGE future, we will use multiple persist shards to back a STORAGE collection. Then,
+//         // we can read in parallel, by distributing shard reading amongst workers.
+//         if worker_index != 0 {
+//             trace!("We are not worker 0, exiting...");
+//             return;
+//         }
 
-        let read = persist_clients
-            .lock()
-            .await
-            .open(metadata.persist_location)
-            .await
-            .expect("could not open persist client")
-            .open_reader::<SourceData, (), mz_repr::Timestamp, mz_repr::Diff>(metadata.data_shard)
-            .await
-            .expect("could not open persist shard");
+//         let read = persist_clients
+//             .lock()
+//             .await
+//             .open(metadata.persist_location)
+//             .await
+//             .expect("could not open persist client")
+//             .open_reader::<SourceData, (), mz_repr::Timestamp, mz_repr::Diff>(metadata.data_shard)
+//             .await
+//             .expect("could not open persist shard");
 
-        let mut snapshot_iter = read
-            .snapshot(as_of.clone())
-            .await
-            .expect("cannot serve requested as_of");
+//         let mut snapshot_iter = read
+//             .snapshot(as_of.clone())
+//             .await
+//             .expect("cannot serve requested as_of");
 
-        // First, yield all the updates from the snapshot.
-        while let Some(next) = snapshot_iter.next().await {
-            yield ListenEvent::Updates(next);
-        }
+//         // First, yield all the updates from the snapshot.
+//         while let Some(next) = snapshot_iter.next().await {
+//             yield ListenEvent::Updates(next);
+//         }
 
-        // Then, listen continuously and yield any new updates. This loop is expected to never
-        // finish.
-        let mut listen = read
-            .listen(as_of)
-            .await
-            .expect("cannot serve requested as_of");
+//         // Then, listen continuously and yield any new updates. This loop is expected to never
+//         // finish.
+//         let mut listen = read
+//             .listen(as_of)
+//             .await
+//             .expect("cannot serve requested as_of");
 
-        loop {
-            for event in listen.next().await {
-                yield event;
-            }
-        }
-    });
+//         loop {
+//             for event in listen.next().await {
+//                 yield event;
+//             }
+//         }
+//     });
 
-    let mut pinned_stream = Box::pin(async_stream);
+//     let mut pinned_stream = Box::pin(async_stream);
 
-    let (timely_stream, token) =
-        crate::source::util::source(scope, "persist_source".to_string(), move |info| {
-            let waker_activator = Arc::new(scope.sync_activator_for(&info.address[..]));
-            let waker = futures_util::task::waker(waker_activator);
-            let activator = scope.activator_for(&info.address[..]);
+//     let (timely_stream, token) =
+//         crate::source::util::source(scope, "persist_source".to_string(), move |info| {
+//             let waker_activator = Arc::new(scope.sync_activator_for(&info.address[..]));
+//             let waker = futures_util::task::waker(waker_activator);
+//             let activator = scope.activator_for(&info.address[..]);
 
-            // There is a bit of a mismatch: listening on a ReadHandle will give us an Antichain<T>
-            // as a frontier in `Progress` messages while a timely source usually only has a single
-            // `Capability` that it can downgrade. We can work around that by using a
-            // `CapabilitySet`, which allows downgrading the contained capabilities to a frontier
-            // but `util::source` gives us both a vanilla `Capability` and a `CapabilitySet` that
-            // we have to keep downgrading because we cannot simply drop the one that we don't
-            // need.
-            let mut current_ts = 0;
+//             // There is a bit of a mismatch: listening on a ReadHandle will give us an Antichain<T>
+//             // as a frontier in `Progress` messages while a timely source usually only has a single
+//             // `Capability` that it can downgrade. We can work around that by using a
+//             // `CapabilitySet`, which allows downgrading the contained capabilities to a frontier
+//             // but `util::source` gives us both a vanilla `Capability` and a `CapabilitySet` that
+//             // we have to keep downgrading because we cannot simply drop the one that we don't
+//             // need.
+//             let mut current_ts = 0;
 
-            move |cap, output| {
-                let mut context = Context::from_waker(&waker);
-                // Bound execution of operator to prevent a single operator from hogging
-                // the CPU if there are many messages to process
-                let timer = Instant::now();
+//             move |cap, output| {
+//                 let mut context = Context::from_waker(&waker);
+//                 // Bound execution of operator to prevent a single operator from hogging
+//                 // the CPU if there are many messages to process
+//                 let timer = Instant::now();
 
-                while let Poll::Ready(item) = pinned_stream.as_mut().poll_next(&mut context) {
-                    match item {
-                        Some(Ok(ListenEvent::Progress(upper))) => match upper.into_option() {
-                            Some(ts) => {
-                                current_ts = ts;
-                                cap.downgrade(&ts);
-                            }
-                            None => return SourceStatus::Done,
-                        },
-                        Some(Ok(ListenEvent::Updates(mut updates))) => {
-                            // This operator guarantees that its output has been advanced by `as_of.
-                            // The persist SnapshotIter already has this contract, so nothing to do
-                            // here.
-                            let cap = cap.delayed(&current_ts);
-                            let mut session = output.session(&cap);
-                            session.give_vec(&mut updates);
-                        }
-                        Some(Err::<_, ExternalError>(e)) => {
-                            // TODO(petrosagg): error handling
-                            panic!("unexpected error from persist {e}");
-                        }
-                        None => return SourceStatus::Done,
-                    }
-                    if timer.elapsed() > YIELD_INTERVAL {
-                        activator.activate();
-                        break;
-                    }
-                }
+//                 while let Poll::Ready(item) = pinned_stream.as_mut().poll_next(&mut context) {
+//                     match item {
+//                         Some(Ok(ListenEvent::Progress(upper))) => match upper.into_option() {
+//                             Some(ts) => {
+//                                 current_ts = ts;
+//                                 cap.downgrade(&ts);
+//                             }
+//                             None => return SourceStatus::Done,
+//                         },
+//                         Some(Ok(ListenEvent::Updates(mut updates))) => {
+//                             // This operator guarantees that its output has been advanced by `as_of.
+//                             // The persist SnapshotIter already has this contract, so nothing to do
+//                             // here.
+//                             let cap = cap.delayed(&current_ts);
+//                             let mut session = output.session(&cap);
+//                             session.give_vec(&mut updates);
+//                         }
+//                         Some(Err::<_, ExternalError>(e)) => {
+//                             // TODO(petrosagg): error handling
+//                             panic!("unexpected error from persist {e}");
+//                         }
+//                         None => return SourceStatus::Done,
+//                     }
+//                     if timer.elapsed() > YIELD_INTERVAL {
+//                         activator.activate();
+//                         break;
+//                     }
+//                 }
 
-                SourceStatus::Alive
-            }
-        });
+//                 SourceStatus::Alive
+//             }
+//         });
 
-    let (ok_stream, err_stream) = timely_stream.ok_err(|x| match x {
-        ((Ok(SourceData(Ok(row))), Ok(())), ts, diff) => Ok((row, ts, diff)),
-        ((Ok(SourceData(Err(err))), Ok(())), ts, diff) => Err((err, ts, diff)),
-        // TODO(petrosagg): error handling
-        _ => panic!("decoding failed"),
-    });
+//     let (ok_stream, err_stream) = timely_stream.ok_err(|x| match x {
+//         ((Ok(SourceData(Ok(row))), Ok(())), ts, diff) => Ok((row, ts, diff)),
+//         ((Ok(SourceData(Err(err))), Ok(())), ts, diff) => Err((err, ts, diff)),
+//         // TODO(petrosagg): error handling
+//         _ => panic!("decoding failed"),
+//     });
 
-    let token = Rc::new(token);
+//     let token = Rc::new(token);
 
-    (ok_stream, err_stream, token)
-}
+//     (ok_stream, err_stream, token)
+// }
 
 pub fn persist_source_listen_split<G>(
     scope: &G,
@@ -284,11 +284,12 @@ where
 
                             // Round robin
                             i = (i + 1) % peers;
-
-                            if let Some(frontier) = progress {
+                            println!("op_0: capability is {:?}", cap);
+                            if let Some(frontier) = dbg!(progress) {
                                 match frontier.into_option() {
                                     Some(ts) => {
                                         cap.downgrade(&ts);
+                                        println!("op_0: capability downgraded to {:?}", cap);
                                         current_ts = ts;
                                     }
                                     None => return SourceStatus::Done,
@@ -343,7 +344,8 @@ where
             let timer = Instant::now();
 
             while let Some((cap, data)) = input.next() {
-                let mut cap = cap.retain();
+                println!("op_1 cap {:?}", cap);
+                // let mut cap = cap.retain();
                 for (_idx, batch) in data.iter() {
                     for update in read
                         .fetch_batch(batch.clone())
