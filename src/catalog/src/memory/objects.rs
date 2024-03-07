d8271f2532e0593e05341d421d77c0c8bf0f4fa7
Sean Loiselle <sean@materialize.io>
2024-03-07 09:24:10 -0500

    catalog: add CatalogItem fn to get source exports
    To migrate a legacy-style source to the new structure, we need
    the ability to get the GlobalId and the output index of the
    exports. This code can be removed in the version after it's added.

--- src/catalog/src/memory/objects.rs
+++ src/catalog/src/memory/objects.rs
@@ -1480,6 +1480,21 @@ impl CatalogEntry {
         }
     }
 
+    /// Returns the mapping of `GloablIds` to output indices for this source.
+    pub fn source_exports(&self) -> BTreeMap<GlobalId, usize> {
+        match &self.item() {
+            CatalogItem::Source(source) => match &source.data_source {
+                DataSourceDesc::Ingestion(ingestion) => ingestion
+                    .source_exports
+                    .iter()
+                    .map(|(id, export)| (*id, export.output_index))
+                    .collect(),
+                _ => BTreeMap::new(),
+            },
+            _ => BTreeMap::new(),
+        }
+    }
+
     /// Reports whether this catalog entry is a progress source.
     pub fn is_progress_source(&self) -> bool {
         self.item().is_progress_source()
@@ -2266,6 +2281,10 @@ impl mz_sql::catalog::CatalogItem for CatalogEntry {
 
     fn is_subsource(&self) -> bool {
         self.is_subsource()
+    }
+
+    fn source_exports(&self) -> BTreeMap<GlobalId, usize> {
+        self.source_exports()
     }
 
     fn is_progress_source(&self) -> bool {
